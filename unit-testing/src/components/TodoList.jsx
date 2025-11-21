d67869f424bd6e5a7c5810607078f3cde0c01406
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-21 11:05:23 +0200

    Update TodoList.jsx
    initialize state
    Render TodoItem

--- unit-testing/src/components/TodoList.jsx
+++ unit-testing/src/components/TodoList.jsx
@@ -1,8 +1,50 @@
-export default function TodoList(){
-    return (
+import { useState } from "react";
+import TodoItem from "./TodoItem";
 
+export default function TodoList(){
+
+    const [todos, setTodos] = useState({
+        "todo_1":{
+            "_id": "todo_2",
+            "text": "Do laundry",
+            "isCompleted": true
+        },
+        "todo_2":{
+            "_id": "todo_1",
+            "text": "Grocery shopping",
+            "isCompleted": false
+        },
+        "todo_3":{
+            "_id": "todo_2",
+            "text": "Do laundry",
+            "isCompleted": true
+        },
+        "todo_4":{
+            "_id": "todo_3",
+            "text": "Finish presentation slides",
+            "isCompleted": false
+        },
+        "todo_5":{
+            "_id": "todo_4",
+            "text": "Call the mechanic",
+            "isCompleted": true
+        },
+        "todo_6":{
+            "_id": "todo_5",
+            "text": "Read chapter 5 of the book",
+            "isCompleted": false
+        }
+        }
+    )
+
+    console.log('Object.values are:', Object.values(todos))
+
+    return (
         <>
-        <h1>TodoList</h1>
+        <ul className="divide-y divide-gray-200 px-4">
+            {Object.values(todos).map((obj) => <TodoItem {...obj} />)}
+            
+        </ul>
         </>
     );
 }
