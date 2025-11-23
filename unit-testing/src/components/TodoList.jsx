acae89a8a79e6d2564a5326700f3166ef40021d6
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-23 12:14:53 +0200

    Update TodoList component
    declare state
    declare endpoint URL
    declare useEffect hook
    fetch database and save response in state
    declare handleChange function
    iterate over state and render TodoItem component
    Pass props to TodoItem component

--- unit-testing/src/components/TodoList.jsx
+++ unit-testing/src/components/TodoList.jsx
@@ -1,48 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
+import request from '../utils/requester'
 
 export default function TodoList(){
 
-    const [todos, setTodos] = useState({
-        "todo_1":{
-            "_id": "todo_2",
-            "text": "Do laundry",
-            "isCompleted": true
-        },
-        "todo_2":{
-            "_id": "todo_1",
-            "text": "Grocery shopping",
-            "isCompleted": false
-        },
-        "todo_3":{
-            "_id": "todo_2",
-            "text": "Do laundry",
-            "isCompleted": true
-        },
-        "todo_4":{
-            "_id": "todo_3",
-            "text": "Finish presentation slides",
-            "isCompleted": false
-        },
-        "todo_5":{
-            "_id": "todo_4",
-            "text": "Call the mechanic",
-            "isCompleted": true
-        },
-        "todo_6":{
-            "_id": "todo_5",
-            "text": "Read chapter 5 of the book",
-            "isCompleted": false
-        }
-        }
-    )
+    const [todos, setTodos] = useState([])
 
-    console.log('Object.values are:', Object.values(todos))
+    const URL = 'http://localhost:3030/data/todos'
+
+    useEffect(() => {
+        request.get(URL)
+            .then((result) => {
+
+                console.log('Result from server is:', Object.values(result))
+                const todosResult = Object.values(result);
+
+                setTodos(todosResult);
+            } )
+    },[])
+    
+    const handleChange = (_id) => {
+        setTodos(state => state.map((todo) => todo._id === _id ? {...todo, isCompleted: !todo.isCompleted} : todo))
+    }
+
+    // console.log('Object.values are:', Object.values(todos))
 
     return (
         <>
         <ul className="divide-y divide-gray-200 px-4">
-            {Object.values(todos).map((obj) => <TodoItem {...obj} />)}
+           {
+           todos.length > 0
+                ?
+        todos.map((todo) => <TodoItem {...todo} key={todo._id} onToggle={handleChange} />)
+                :
+                ""
+           }
             
         </ul>
         </>
