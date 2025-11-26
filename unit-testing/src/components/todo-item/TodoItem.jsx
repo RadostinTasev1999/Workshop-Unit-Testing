3fe93d4868a343addc8dcbad35d6fef364afbc09
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-26 08:01:19 +0200

    change TodoItem.jsx file location

---
+++ unit-testing/src/components/todo-item/TodoItem.jsx
@@ -0,0 +1,47 @@
+// import { useState } from "react"
+
+export default function TodoItem({
+  _id,
+  text,
+  isCompleted,
+  onToggle
+}) {
+
+  // const [input, setInput] = useState(() => {
+  //     return isCompleted
+  // })
+
+  // const handleChange = () => {
+  //   //const target = e.target
+  //   const isChecked = isCompleted ? false : true
+
+  //   setInput(isChecked)
+  // }
+
+  
+  
+  return (
+    <>
+    <li className="py-4">
+      <div className="flex items-center">
+        <input
+          id="todo1"
+          name="todo1"
+          type="checkbox"
+          checked={isCompleted}
+          onChange={() => onToggle(_id)}
+          className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"
+        />
+        <label htmlFor="todo1" className="ml-3 block text-gray-900">
+          <span className="text-lg font-medium">{text}</span>
+          <span className="text-sm font-light text-gray-500">
+            Due on 4/1/23
+          </span>
+        </label>
+      </div>
+    </li>
+    </>  
+  )
+}
+
+
