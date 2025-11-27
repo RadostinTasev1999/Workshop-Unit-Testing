e62f853d8fa040e843bc914da05b2ee864f9f938
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-27 07:59:02 +0200

    Update TodoItem.spec.js
    declare beforeEach hook
    
    create test cases

--- unit-testing/src/components/todo-item/TodoItem.spec.js
+++ unit-testing/src/components/todo-item/TodoItem.spec.js
@@ -1,20 +1,28 @@
-import { it, expect } from 'vitest'
+import { it, expect, beforeEach } from 'vitest'
 import ReactDOM from 'react-dom/client'
 import TodoItem from './TodoItem'
 import { act } from 'react'
+import { cleanup, getByText, render } from '@testing-library/react'
 
 // it('Should be true',() => {
 //     expect(true).toBe(true)
 // })
 
+beforeEach(() => {
+    document.body.innerHTML = '';
+
+    cleanup();
+});
+
 
 it('Should display todo text', async() => {
-    const container = document.createElement('div'); // create container
 
-    document.body.appendChild(container) // add container to document body
+    const container = document.createElement('div')
 
-    const root = ReactDOM.createRoot(container) // create root element
+    document.body.appendChild(container)
 
+    const root = ReactDOM.createRoot(container)
+    
     await act(() => {
         root.render(<TodoItem text="test-1"/>) // render component to React Root element
     })
@@ -23,4 +31,14 @@ it('Should display todo text', async() => {
     expect(textElement.textContent).toEqual("test-1")
 
     //expect(true).toBe(true)
+})
+
+it('Should display todo text',() => {
+    render(<TodoItem text="test1"/>)
+
+    //const textElement = document.querySelector('label[for=todo1] span') // get textElement
+
+    const textElement = getByText(document, 'test1') // find by element text content
+
+    expect(textElement).not.toBeNull() 
 })
