0b04b865e55de4512ef37c9bc285980a8b45cfd9
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-12-01 08:03:20 +0200

    Update setup.js

--- unit-testing/src/tests/setup.js
+++ unit-testing/src/tests/setup.js
@@ -1,5 +1,6 @@
 import { cleanup } from '@testing-library/react'
 import { beforeAll } from 'vitest'
+import ReactDOM from 'react-dom/client'
 import '@testing-library/jest-dom/vitest'
 
 beforeAll(() => {
