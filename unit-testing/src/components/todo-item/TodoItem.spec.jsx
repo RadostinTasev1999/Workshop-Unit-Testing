ddd19bd0fb1a3633efebcb17fde5528bed5cbc1e
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-12-01 08:01:06 +0200

    Update TodoItem.spec.jsx
    Create a new test case

--- unit-testing/src/components/todo-item/TodoItem.spec.jsx
+++ unit-testing/src/components/todo-item/TodoItem.spec.jsx
@@ -1,46 +1,75 @@
-import { it, expect, beforeEach } from 'vitest'
+
 import ReactDOM from 'react-dom/client'
 import TodoItem from './TodoItem'
-import { act } from 'react'
-import { cleanup, getByText, render } from '@testing-library/react'
+import { it, expect, vi } from 'vitest'
+import { fireEvent } from '@testing-library/dom'
+import { render, screen } from '@testing-library/react'
+// import { screen } from '@testing-library/react'
+// import { cleanup, getByText, render } from '@testing-library/react
+// import { render } from '@testing-library/react'
 
 
 
-it('Should display todo text', async() => {
+// it('Should display todo text', async() => {
 
-    const container = document.createElement('div')
+//     const container = document.createElement('div') // create container
 
-    document.body.appendChild(container)
+//     document.body.appendChild(container) //
 
-    const root = ReactDOM.createRoot(container)
+//     const root = ReactDOM.createRoot(container) // create a root to display React components inside a browser DOM node.
     
-    await act(() => {
-        root.render(<TodoItem text="test-1"/>) // render component to React Root element
-    })
-    const textElement = document.querySelector('label[for=todo1] span') // get textElement
+//     await act(() => {
+//         root.render(<TodoItem text="test-1"/>) // render component to React Root element
+//     })
 
-    expect(textElement.textContent).toEqual("test-1")
+//     // Using act ensures that all updates have been applied before we make assertions.
+//     const textElement = document.querySelector('label[for=todo1] span') // get textElement
 
-    //expect(true).toBe(true)
-})
+//     expect(textElement.textContent).toEqual("test-1")
 
-it('Should display todo text',() => {
-    render(<TodoItem text="test1"/>)
+//     //expect(true).toBe(true)
+// })
 
-    //const textElement = document.querySelector('label[for=todo1] span') // get textElement
+// it('Should display todo text',async() => {
 
-    const textElement = screen.queryByText('test1')
+//     // await act(() => {
+//     //     render(<TodoItem text="test1"/>)
+//     // })
 
-    // getByText(document, 'test1') // find by element text content
+//     render(<TodoItem text="test-1" />)
 
-    expect(textElement).toBeInTheDocument(); 
-})
+//     const textElement = screen.queryByText('test-1') //  find by element text content
 
-it('Should be checked when is completed', () => {
-    render(<TodoItem isCompleted={true} />)
+//     // getByText(document, 'test1') // find by element text content
 
-    const checkbox = screen.queryByRole('checkbox', { name: 'todo1' })
+//     expect(textElement).toBeInTheDocument(); 
+// })
 
-    expect(checkbox).toBeChecked()
+// it('Should be checked when is completed', () => {
+//     render(<TodoItem isCompleted={true} />)
 
+//     const checkbox = screen.queryByRole('checkbox', { name: 'todo1' })
+
+//     expect(checkbox).toBeChecked()
+
+// })
+
+
+it('Should be checked when clicked',() => {
+    const onToggle = vi.fn()
+
+    render(
+        <TodoItem 
+            isCompleted={false}
+            text="checkbox-1" 
+            onToggle={onToggle}
+            />)
+
+    const checkbox = screen.getByRole('checkbox', { name: 'checkbox-1' })
+
+    fireEvent.click(checkbox)
+    
+    expect(onToggle).toBeCalled();
+    expect(onToggle).toBeCalledTimes(1);
+    // screen.debug()
 })
