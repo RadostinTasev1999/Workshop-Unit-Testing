992950b2b5b839d80c02f16504aeaa2a35249ab6
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-12-04 09:15:01 +0200

    Update TodoList.spec.jsx
    Add a new test case

--- unit-testing/src/components/todo-list/TodoList.spec.jsx
+++ unit-testing/src/components/todo-list/TodoList.spec.jsx
@@ -1,7 +1,42 @@
-import { it } from 'vitest'
-import { render } from '@testing-library/react'
+import { it,expect, vi } from 'vitest'
+import { fireEvent, render } from '@testing-library/react'
 import TodoList from './TodoList'
 
-it('Should check todo when clicked',() => {
+// it('Should check todo when clicked | Snapshot',() => {
+//     render(<TodoList />)
+
+//     expect(true).toBe(true)
+// })
+
+it('Should check todo when clicked | Snapshot', () => {
+
+    /*
+    Mocked result which we expect the fake requester to return
+    */
+    const mockResult = {
+        'asd' :{
+            _id: 'id1',
+            text: 'text1',
+            isCompleted: false  
+        }
+    }
+
+    vi.mock('../../utils/requester.js',async (importOriginal) => {
+        const request = await importOriginal()
+
+        return {
+            ...request,
+            async get() {
+                return mockResult
+            }
+        }
+    })
+
     render(<TodoList />)
+
+    vi.waitUntil()
+
+    screen.debug()
+
+    expect(true).toBe(true)
 })
